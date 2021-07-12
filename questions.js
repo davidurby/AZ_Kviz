bc072c2281d0ef700adf135d9aca97f6a1d4dfd7
PetrLhotsky <petr.lhotsky@outlook.cz>
2021-07-12 14:16:34 +0200

    commit

---
+++ questions.js
@@ -0,0 +1,405 @@
+        var content = [
+            {
+                question: "Jaká je nejvyšší hora České republiky?",
+                answer: "Sněžka"
+            },
+            {
+                question: "Která řeka protéká hlavním městem?",
+                answer: "Vltava"
+            },
+            {
+                question: "Kdo zavedl povinnou školní docházku?",
+                answer: "Marie Terezie"
+            },
+            {
+                question: "Která spisovatelka napsala Babičku?",
+                answer: "Božena Němcová"
+            },
+            {
+                question: "S kterým státem má ČR nejdelší hranici?",
+                answer: "Německo"
+            },
+            {
+                question: "Kdo jinému jámu kopá, sám... doplňte",
+                answer: "do ní padá"
+            },
+            {
+                question: "Jaké je hlavní město Polska?",
+                answer: "Varšava"
+            },
+            {
+                question: "Jaké je hlavní město Slovenska?",
+                answer: "Bratislava"
+            },
+            {
+                question: "Jaké je hlavní město Rakouska?",
+                answer: "Vídeň"
+            },
+            {
+                question: "Jaké je hlavní město Německa?",
+                answer: "Berlín"
+            },
+            {
+                question: "Jaké je hlavní město Spojených států amerických?",
+                answer: "Washington"
+            },
+            {
+                question: "Jaké je hlavní město Ruska?",
+                answer: "Moskva"
+            },
+            {
+                question: "Jaké je hlavní město Číny?",
+                answer: "Peking"
+            },
+            {
+                question: "Která hora je nejvyšší horou světa?",
+                answer: "Mount Everest"
+            },
+            {
+                question: "Která hora je nejvyšší horou Evropy?",
+                answer: "Mont Blanc"
+            },
+            {
+                question: "Která řeka je nejdelší řekou světa?",
+                answer: "Amazonka"
+            },
+            {
+                question: "Která řeka je nejdelší řekou Evropy?",
+                answer: "Volha"
+            },
+            {
+                question: "Ve kterém měsíci je Den obnovy samostatného českého státu?",
+                answer: "leden"
+            },
+            {
+                question: "Ve kterém měsíci je Den vítězství?",
+                answer: "květen"
+            },
+            {
+                question: "Ve kterém měsíci je Den slovanských věrozvěstů Cyrila a Metoděje?",
+                answer: "červenec"
+            },
+            {
+                question: "Ve kterém měsíci je Den upálení mistra Jana Husa?",
+                answer: "červenec"
+            },
+            {
+                question: "Ve kterém měsíci je Den české státnosti?",
+                answer: "září"
+            },
+            {
+                question: "Ve kterém měsíci je Den vzniku samostatného československého státu?",
+                answer: "říjen"
+            },
+            {
+                question: "Ve kterém měsíci je Den boje za svobodu a demokracii a Mezinárodní den studenstva?",
+                answer: "listopad"
+            },
+            {
+                question: "Který spisovatel napsal Smrt krásných srnců?",
+                answer: "Ota Pavel"
+            },
+            {
+                question: "Která spisovatelka napsala knihy o Harry Potterovi?",
+                answer: "Joanne Rowlingová"
+            },
+            {
+                question: "Který spisovatel napsal Těžkou hodinu?",
+                answer: "Jiří Wolker"
+            },
+            {
+                question: "Který spisovatel napsal Kytici?",
+                answer: "Karel Jaromír Erben"
+            },
+            {
+                question: "Do kterého řádu třídy savci patří lidé?",
+                answer: "primáti"
+            },
+            {
+                question: "Ve kterém kraji leží Praha?",
+                answer: "Hlavní město Praha"
+            },
+            {
+                question: "Ve kterém kraji leží Hradec Králové?",
+                answer: "Královéhradecký"
+            },
+            {
+                question: "Ve kterém kraji leží Pardubice?",
+                answer: "Pardubický"
+            },
+            {
+                question: "Bez práce... doplňte",
+                answer: "nejsou koláče"
+            },
+            {
+                question: "Kdo chce kam, ... doplňte",
+                answer: "pomozme mu tam"
+            },
+            {
+                question: "Kdo nic nedělá, ... doplňte",
+                answer: "nic nezkazí"
+            },
+            {
+                question: "Mluviti stříbro, ... doplňte",
+                answer: "mlčeti zlato"
+            },
+            {
+                question: "Jak se jmenovala říše na našem území v letech 623 až 658?",
+                answer: "Sámova říše"
+            },
+            {
+                question: "Která nejvýznamnější smlouva byla uzavřena roku 1212?",
+                answer: "Zlatá bula sicilská"
+            },
+            {
+                question: "Jaké jsou dvě hlavní vědy přírodopisu (oddělte mezerou)?",
+                answer: "biologie geologie"
+            },
+            {
+                question: "Který plyn je v zemské atmosféře obsažen nejvíce?",
+                answer: "dusík"
+            },
+            {
+                question: "Jak se nazývá plynný obal země?",
+                answer: "atmosféra"
+            },
+            {
+                question: "Jak se nazývá vodní obal země?",
+                answer: "hydrosféra"
+            },
+            {
+                question: "Jak se nazývá kamenný obal země?",
+                answer: "litosféra"
+            },
+            {
+                question: "Jak se nazývá půdní obal země?",
+                answer: "pedosféra"
+            },
+            {
+                question: "Jak se nazývá živý obal země?",
+                answer: "biosféra"
+            },
+            {
+                question: "Jak se nazývá teorie vzniku vesmíru?",
+                answer: "Velký třesk"
+            },
+            {
+                question: "Jak se nazývá horní část zlomku?",
+                answer: "čitatel"
+            },
+            {
+                question: "Jak se nazývá dolní část zlomku?",
+                answer: "jmenovatel"
+            },
+            {
+                question: "Která barva vznikne smícháním modré a žluté?",
+                answer: "zelená"
+            },
+            {
+                question: "Které zařízení umožňuje zadávat text do počítače?",
+                answer: "klávesnice"
+            },
+        ];
+
+        var spare = [
+            {
+                question: "Je číslo 1 prvočíslem?",
+                correct: false
+            },
+            {
+                question: "Je číslo 2 prvočíslem?",
+                correct: true
+            },
+            {
+                question: "Platí rovnice 5 + 8 = 14 - 5 * 0,2?",
+                correct: true
+            },
+            {
+                question: "Platí rovnice 7 * 8 = 2 * 3 * 7?",
+                correct: false
+            },
+            {
+                question: "Pocházel Archimédés z Řecka?",
+                correct: true
+            },
+            {
+                question: "Pocházel Julius Caesar z Řecka?",
+                correct: false
+            },
+            {
+                question: "Je výraz \"plyvat\" pravopisně správně?",
+                correct: false
+            },
+            {
+                question: "Je výraz \"vodní výr\" pravopisně správně?",
+                correct: false
+            },
+            {
+                question: "Je výraz \"děti slyšely\" pravopisně správně?",
+                correct: true
+            },
+            {
+                question: "Hraje při fotbale v jednom mužstvu 11 hráčů?",
+                correct: true
+            },
+            {
+                question: "Hraje při hokeji v jednom týmu 5 hráčů?",
+                correct: false
+            },
+            {
+                question: "Konaly se LOH 2012 v Londýně?",
+                correct: true
+            },
+            {
+                question: "Konaly se LOH 2016 v Buenos Aires?",
+                correct: false
+            },
+            {
+                question: "Konaly se ZOH 2014 v Soči?",
+                correct: true
+            },
+            {
+                question: "Konaly se ZOH 2018 v Severní Koreji?",
+                correct: false
+            },
+            {
+                question: "Velryba je savec. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Plejtvák je savec. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Delfín je savec. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Mlok skvrnitý je plaz. Je to pravda",
+                correct: false
+            },
+            {
+                question: "Čolek obecný je plaz. Je to pravda?",
+                correct: false
+            },
+            {
+                question: "Krokodýli jsou obojživelníci. Je to pravda?",
+                correct: false
+            },
+            {
+                question: "Želvy jsou obojživelníci. Je to pravda?",
+                correct: false
+            },
+            {
+                question: "Vznikl film Harry Potter a kámen mudrců roku 2011?",
+                correct: false
+            },
+            {
+                question: "Vznikl film Harry Potter a Fénixův řád roku 2009?",
+                correct: false
+            },
+            {
+                question: "Překonal film Avatar tržby 3 000 000 000?",
+                correct: false
+            },
+            {
+                question: "Získal Karel Čapek Nobelovu cenu za literaturu?",
+                correct: false
+            },
+            {
+                question: "Získal Jaroslav Heyrovský Nobelovu cenu za chemii?",
+                correct: true
+            },
+            {
+                question: "Mohli se potkat L. v. Beethoven a W. A. Mozart?",
+                correct: true
+            },
+            {
+                question: "Mohli se potkat Jan Hus a Karel IV.?",
+                correct: true
+            },
+            {
+                question: "Mohli se potkat Tomáš Garrigue Masaryk a Adolf Hitler?",
+                correct: true
+            },
+            {
+                question: "Mohli se potkat Julius Caesar a Pythagoras?",
+                correct: false
+            },
+            {
+                question: "Česko mělo od roku 1993 jen 3 prezidenty. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Slovensko mělo od roku 1993 4 prezidenty. Je to pravda?",
+                correct: false
+            },
+            {
+                question: "Burundi je stát v Africe. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Vanuatu je stát v Jižní Americe. Je to pravda?",
+                correct: false
+            },
+            {
+                question: "Afrika je největším kontinentem na Zemi. je to pravda?",
+                correct: false
+            },
+            {
+                question: "Hlavní město Autrálie je Sydney. Je to pravda?",
+                correct: false
+            },
+            {
+                question: "Hlavní město Kanady je Montréal. Je to pravda?",
+                correct: false
+            },
+            {
+                question: "Čeština patří do slovanských jazyků. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Angličtina patří do germánských jazyků. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Italština patří do románských jazyků. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Je druhá mocnina jakéhokoli čísla vždy nezáporná?",
+                correct: true
+            },
+            {
+                question: "Je třetí odmocnina záporného čísla vždy kladná?",
+                correct: false
+            },
+            {
+                question: "Je souvětí se spojkou \"a\" souřadné?",
+                correct: true
+            },
+            {
+                question: "Je souvětí se spojkou \"ale\" souřadné?",
+                correct: true
+            },
+            {
+                question: "Je souvětí se spojkou \"když\" souřadné?",
+                correct: false
+            },
+            {
+                question: "Slovesa dělíme dle koncovky do pěti tříd. Je to pravda?",
+                correct: true
+            },
+            {
+                question: "Podstatná jména dělíme na dva rody. Je to pravda?",
+                correct: false
+            },
+            {
+                question: "Výraz \"actually\" v AJ znamená \"aktuálně\". Je to pravda?",
+                correct: false
+            },
+            {
+                question: "V němčině se používá takzvané onikání. Je to pravda?",
+                correct: true
+            },
+        ];
